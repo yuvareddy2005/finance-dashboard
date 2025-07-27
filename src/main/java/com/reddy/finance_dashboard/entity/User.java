421eaf869386f351aa8be505e0a98cb559b087a3
yuvareddy2005 <yuvareddy2005@gmail.com>
2025-07-27 09:24:12 +0530

    feat: Add User entity and repository, auto-create users table

---
+++ src/main/java/com/reddy/finance_dashboard/entity/User.java
@@ -0,0 +1,33 @@
+package com.reddy.finance_dashboard.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+}
---
+++ src/main/java/com/reddy/finance_dashboard/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.reddy.finance_dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.reddy.finance_dashboard.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
