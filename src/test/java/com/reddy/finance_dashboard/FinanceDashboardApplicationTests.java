4f3455451522d1e4d611765e0ee24534e61a5080
yuvareddy2005 <yuvareddy2005@gmail.com>
2025-07-27 09:07:10 +0530

    feat: Initial project setup and successful database connection

---
+++ src/test/java/com/reddy/finance_dashboard/FinanceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddy.finance_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
