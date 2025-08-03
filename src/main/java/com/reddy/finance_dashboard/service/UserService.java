d89b292fd4d453d1071aae4c5102ad1053fdeaac
yuvareddy2005 <yuvareddy2005@gmail.com>
2025-08-03 19:09:08 +0530

    feat: Automatically create a portfolio for new users

--- src/main/java/com/reddy/finance_dashboard/service/UserService.java
+++ src/main/java/com/reddy/finance_dashboard/service/UserService.java
@@ -1,11 +1,14 @@
 package com.reddy.finance_dashboard.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder; // <-- ADD THIS IMPORT
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.reddy.finance_dashboard.dto.UserRegistrationRequest;
+import com.reddy.finance_dashboard.entity.Portfolio;
 import com.reddy.finance_dashboard.entity.User;
+import com.reddy.finance_dashboard.repository.PortfolioRepository;
 import com.reddy.finance_dashboard.repository.UserRepository;
 
 @Service
@@ -15,18 +18,26 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder; // <-- INJECT THE ENCODER
+    private PortfolioRepository portfolioRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional // Add this annotation
     public User createUser(UserRegistrationRequest registrationRequest) {
+        // Create and save the user
         User newUser = new User();
         newUser.setFirstName(registrationRequest.getFirstName());
         newUser.setLastName(registrationRequest.getLastName());
         newUser.setEmail(registrationRequest.getEmail());
+        newUser.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
         
-        // v-- HASH THE PASSWORD BEFORE SETTING IT --v
-        String hashedPassword = passwordEncoder.encode(registrationRequest.getPassword());
-        newUser.setPassword(hashedPassword);
+        User savedUser = userRepository.save(newUser);
 
-        return userRepository.save(newUser);
+        // Create and save a portfolio for the new user
+        Portfolio newPortfolio = new Portfolio(savedUser);
+        portfolioRepository.save(newPortfolio);
+
+        return savedUser;
     }
 }
